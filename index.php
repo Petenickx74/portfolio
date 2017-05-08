118684d27ddfc85a47cb129163692f34a2b18e37
Petenickx74 <peternickell@gmail.com>
2017-05-08 16:56:31 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,129 @@
 <!DOCTYPE html>
-<html>
-	<head>
-	</head>
-	<body>
+<html lang="en">
+<head>
+	<title>Peter Nickell - Portfolio</title>
+	
+	<meta charset="utf-8">
+	<meta name="author" content="Peter Nickell">
+	<meta name="description" content="Portfolio"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css"
+          href="https://fonts.googleapis.com/css?family=Rock Salt">
+    	<style>
+      body {
+        font-family: 'Rock Salt', cursive;
+        font-size: 40px;
+      }
+    </style>
+	<script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+	<!-- Tab Image -->
+        <link rel="shortcut icon" href="https://github.com/Petenickx74/portfolio/blob/gh-pages/img/Headshotsm.png?raw=true" />
+</head>
+<body>
 
-<?php
-include("portfolio/includes/Home.html");
-include("portfolio/includes/header.html");
-}else if($_GET['page'] == "About"){
-include("portfolio/includes/About.html");
-}else if($_GET['page'] == "Blog"){
-include("portfolio/includes/Blog.html");
-}else if($_GET['page'] == "Shop"){
-include("portfolio/includes/Shop.html");
-}else if($_GET['page'] == "Contact"){
-include("portfolio/includes/Contact.html");
-if($_GET['page'] == "Sketchbook"){
-include("portfolio/includes/Sketchbook.html");
-}else if($_GET['page'] == "Painting&Color"){
-include("portfolio/includes/Painting&Color.html");
-}else if($_GET['page'] == "CommericalArt"){
-include("portfolio/includes/CommericalArt.html");
-}else if($_GET['page'] == "Music"){
-include("portfolio/includes/Music.html");
-}else if($_GET['page'] == "Games"){
-include("portfolio/includes/Games.html");
-}else if($_GET['page'] == "Video"){
-include("portfolio/includes/Video.html");
-?>
+	<header>
+		<div class="logo">
+			 <div style="font-family: 'Rock Salt', cursive;">Peter Nickell</div>
+		</div><!-- end logo -->
+
+		<div id="menu_icon"></div>
+		<nav>
+			<ul>
+				<li><a href="index.html" class="selected">Home</a></li>
+				<li><a href="About.html">About</a></li>
+				<li><a href="Blog.html">Blog</a></li>
+				<li><a href="#">Shop</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</nav><!-- end navigation menu -->
+
+		<div class="footer clearfix">
+			<ul class="social clearfix">
+				<li><a href="#" class="fb" data-title="Facebook"></a></li>
+				<li><a href="#" class="google" data-title="Google +"></a></li>
+				<li><a href="#" class="behance" data-title="Behance"></a></li>
+				<!--<li><a href="#" class="twitter" data-title="Twitter"></a></li>
+				<li><a href="#" class="dribble" data-title="Dribble"></a></li>-->
+				<li><a href="#" class="rss" data-title="RSS"></a></li>
+			</ul><!-- end social -->
+
+			<div class="rights">
+				<p>Copyright © 2023 Peter Nickell</p>
+			</div><!-- end rights -->
+		</div ><!-- end footer -->
+	</header><!-- end header -->
+
+	<section class="main clearfix">
+		<div class="work">
+			<a href="Sketchbook.html">
+				<img src="https://github.com/Petenickx74/portfolio/blob/gh-pages/img/ferrymancropped.jpg?raw=true"class="media"  height="325"  alt=""/>
+				<div class="caption">
+					<div class="work_title">
+						<h1>Sketchbook</h1>
+					</div>
+				</div>
+			</a>
+		</div>
+
+		<div class="work">
+			<a href="Painting&Color.html">
+				<img src="https://github.com/Petenickx74/portfolio/blob/gh-pages/img/Nude1cropped.jpg?raw=true"  height="325" class="media" alt=""/>
+				<div class="caption">
+					<div class="work_title">
+						<h1>Painting & Color</h1>
+					</div>
+				</div>
+			</a>
+		</div>
+
+		<div class="work">
+			<a href="CommercialArt.html">
+				<img src="https://github.com/Petenickx74/portfolio/blob/gh-pages/img/cover2cropped.jpg?raw=true"  height="325" class="media" alt=""/>
+				<div class="caption">
+					<div class="work_title">
+						<h1>Commercial Art</h1>
+					</div>
+				</div>
+			</a>
+		</div>
+
+		<div class="work">
+			<a href="Music.html">
+				<img src="https://github.com/Petenickx74/portfolio/blob/gh-pages/img/musiccropped.png?raw=true"  height="325" class="media" alt=""/>
+				<div class="caption">
+					<div class="work_title">
+						<h1>Music</h1>
+					</div>
+				</div>
+			</a>
+		</div>
+
+		<div class="work">
+			<a href="Games.html">
+				<img src="https://github.com/Petenickx74/portfolio/blob/gh-pages/img/ShadowDencropped.jpg?raw=true" class="media" alt=""/>
+				<div class="caption">
+					<div class="work_title">
+						<h1>Games</h1>
+					</div>
+				</div>
+			</a>
+		</div>
+
+		<div class="work">
+			<a href="Video.html">
+				<img src="https://github.com/Petenickx74/portfolio/blob/gh-pages/img/Screen%20Shot%20Vidcropped.png?raw=true"  height="325" class="media" alt=""/>
+				<div class="caption">
+					<div class="work_title">
+						<h1>Video</h1>
+					</div>
+				</div>
+			</a>
+		</div>
+
+	</section><!-- end main -->
+	
 </body>
 </html>
