2751662c613b7603f377cb8c833721dad2571ff4
Peter Nickell <peternickell@gmail.com>
2017-05-12 12:16:42 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -42,7 +42,7 @@ jQuery(function($){
 			  }
 			];	
 			var infowindow = new google.maps.InfoWindow({
-				content:"<div class='map_adresse'><div class='map_address'><span class='address'>Address : </span>320 Turk Street San Francisco, CA 94102</div> <div class='map_tel'><span class='tel'>Phone : </span>(207) 423-4249</div></div>"
+				content:"<div class='map_email'><div class='text'><span class='text'>Located in the heart of SF</span></div><div class='map_email'><span class='email'>Email : </span>peternickell@gmail.com</div> <div class='map_tel'><span class='tel'>Phone : </span>(207) 423-4249</div></div>"
 			});	
 			
 			map.setOptions({styles: styles});
